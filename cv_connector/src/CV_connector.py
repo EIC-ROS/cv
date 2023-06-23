b0d3e95c221a588fdacdba7faa3a484a8bb39d92
walkie the robot <robocup.cu@gmail.com>
2023-06-23 17:33:07 +0700

    update cv pkg

--- cv_connector/src/CV_connector.py
+++ cv_connector/src/CV_connector.py
@@ -13,10 +13,12 @@ class CVConnector:
     bridge = CvBridge()
 
     def __init__(self):
+
+        # rospy.wait_for_message("/zed2i/zed_node/rgb/camera_info",CameraInfo)
         camera_info = message_filters.Subscriber("/zed2i/zed_node/rgb/camera_info",CameraInfo)
         camera_image = message_filters.Subscriber("/zed2i/zed_node/rgb/image_rect_color",Image)
         message_synchronizer = message_filters.TimeSynchronizer([camera_info,camera_image],queue_size=1)
-        message_synchronizer.callbacks(self.image_callback)
+        message_synchronizer.registerCallback(self.image_callback)
 
         self.cv_server = rospy.Service("/CV_connect/req_cv",CV_srv,self.cv_request)
         self.__open_socket()
@@ -24,8 +26,8 @@ class CVConnector:
     def __open_socket(self):
         host = socket.gethostname()
 
-        self.client_yolov8      = CustomSocket(host, 10000)
-        self.client_facedetect  = CustomSocket(host, 10001)
+        self.client_yolov8      = CustomSocket(host, 12301)
+        self.client_facedetect  = CustomSocket(host, 12304)
 
         self.client_yolov8.clientConnect()
         self.client_facedetect.clientConnect()
@@ -36,11 +38,10 @@ class CVConnector:
 
     def cv_request(self, cv_req:CV_srvRequest):
         error = False
-        cv_type = CV_srvRequest.CV_type
+        cv_type = CV_srvRequest().CV_type
 
         image = self.image
-
-        # return CV_srvResponse(False)
+        info  = self.info
 
         try:
             cv_image = self.bridge.imgmsg_to_cv2(image,desired_encoding="bgr8")
@@ -53,12 +54,31 @@ class CVConnector:
             error = True
         
 
+        print(CV_type.YoloV8_Tracker)
+        print(cv_type)
 
-
-        if cv_type == CV_type.YOLOV8:
+        if cv_type.type == CV_type.YoloV8_Tracker:
             res = self.client_yolov8.req(pull_image)
-        elif cv_type == CV_type.FACE_RECONISION:
-            pass
+        elif cv_type.type == CV_type.Face_Recognition:
+            res = self.client_facedetect.req(pull_image)
         else:
             rospy.logerr("Error CV_type: %s" % (cv_type))
             error = True
+
+        print(res)
+
+        srv_res = CV_srvResponse()
+        if not error:
+            srv_res.sucess = True
+            srv_res.result = res
+            srv_res.camera_info = info
+        
+        else:
+            srv_res.sucess = False
+        
+        return srv_res
+    
+if __name__ == "__main__":
+    rospy.init_node("cv_connect_test")
+    cv_con = CVConnector()
+    rospy.spin()
